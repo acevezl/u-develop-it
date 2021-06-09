82d33e62573f863c97d80d4650197a66490d6d1c
Arnaut <lou.arnaut@gmail.com>
2021-06-08 17:13:20 -0700

    Added CRUD queries

--- server.js
+++ server.js
@@ -20,11 +20,37 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: `It's alive!`
-//     });
-// });
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+        console.log(rows);
+});
+
+// Delete a candidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+});
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Virginia', 'Woolf', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
+// GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(row);
+});
 
 // Default response for not found
 app.use((req,res)=> {
